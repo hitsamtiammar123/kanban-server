d383f5bce7abe25bd25f01768cf40340f600c6e0
hitsamtiammar123 <hitsamtiammmar@gmail.com>
2020-03-09 15:42:11 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express=require('express');
+const app=express();
+const cors=require('cors');
+const route=require('./route')
+
+const PORT=process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cors());
+
+app.use(route);
+
+app.listen(PORT,()=>{
+    console.log('Server jalan di port '+PORT);
+})
